8467970c2f238ec1eb211f776ffecb192bd620d8
veshon <vashonmabima12@gmail.com>
2024-10-04 18:26:32 +0530

    Delete and Update Customer Done

--- src/main/java/com/example/assignment2/controller/CustomerController.java
+++ src/main/java/com/example/assignment2/controller/CustomerController.java
@@ -3,6 +3,8 @@ package com.example.assignment2.controller;
 import com.example.assignment2.customStatusCode.SelectedUserAndNoteErrorStatus;
 import com.example.assignment2.dto.CustomerStatus;
 import com.example.assignment2.dto.impl.CustomerDTO;
+import com.example.assignment2.entity.impl.CustomerEntity;
+import com.example.assignment2.exception.CustomerNotFoundException;
 import com.example.assignment2.exception.DataPersistException;
 import com.example.assignment2.srvice.CustomerService;
 import com.example.assignment2.util.AppUtil;
@@ -13,6 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.regex.Pattern;
 
 @RestController
@@ -28,28 +31,67 @@ public class CustomerController {
         try {
             customerService.saveCustomer(noteDTO);
             return new ResponseEntity<>(HttpStatus.CREATED);
-        }catch (DataPersistException e){
+        } catch (DataPersistException e) {
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<CustomerDTO> getAllCustomers(){
+    public List<CustomerDTO> getAllCustomers() {
         return customerService.getAllCustomers();
     }
 
-    @GetMapping(value = "/{cusId}",produces = MediaType.APPLICATION_JSON_VALUE)
-    public CustomerStatus getSelectedCustomer(@PathVariable ("cusId") String cusId){
+    @GetMapping(value = "/{cusId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public CustomerStatus getSelectedCustomer(@PathVariable("cusId") String cusId) {
         String regexForUserID = "^CUSTOMER-[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$";
         Pattern regexPattern = Pattern.compile(regexForUserID);
         var regexMatcher = regexPattern.matcher(cusId);
         if (!regexMatcher.matches()) {
-            return new SelectedUserAndNoteErrorStatus(1,"Customer ID is not valid");
+            return new SelectedUserAndNoteErrorStatus(1, "Customer ID is not valid");
         }
         return customerService.getCustomer(cusId);
+    }
+
+    @DeleteMapping(value = "/{cusId}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable("cusId") String cusId) {
+        String regexForUserID = "^CUSTOMER-[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$";
+        Pattern regexPattern = Pattern.compile(regexForUserID);
+        var regexMatcher = regexPattern.matcher(cusId);
+        try {
+            if (!regexMatcher.matches()) {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+            customerService.deleteCustomer(cusId);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (CustomerNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping(value = "/{cusId}")
+    public ResponseEntity<Void> updateCustomer(@PathVariable ("cusId") String cusId,
+                                           @RequestBody CustomerDTO updatedNCustomerDTO){
+        String regexForUserID = "^CUSTOMER-[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$";
+        Pattern regexPattern = Pattern.compile(regexForUserID);
+        var regexMatcher = regexPattern.matcher(cusId);
+        try {
+            if(!regexMatcher.matches() || updatedNCustomerDTO == null){
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+            customerService.updateCustomer(cusId,updatedNCustomerDTO);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }catch (CustomerNotFoundException e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- src/main/java/com/example/assignment2/srvice/impl/CustomerServiceIMPL.java
+++ src/main/java/com/example/assignment2/srvice/impl/CustomerServiceIMPL.java
@@ -5,6 +5,7 @@ import com.example.assignment2.dao.CustomerDAO;
 import com.example.assignment2.dto.CustomerStatus;
 import com.example.assignment2.dto.impl.CustomerDTO;
 import com.example.assignment2.entity.impl.CustomerEntity;
+import com.example.assignment2.exception.CustomerNotFoundException;
 import com.example.assignment2.exception.DataPersistException;
 import com.example.assignment2.srvice.CustomerService;
 import com.example.assignment2.util.AppUtil;
@@ -14,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -53,11 +55,23 @@ public class CustomerServiceIMPL implements CustomerService {
 
     @Override
     public void deleteCustomer(String cusId) {
-
+        Optional<CustomerEntity> foundCustomer = customerDAO.findById(cusId);
+        if (!foundCustomer.isPresent()) {
+            throw new CustomerNotFoundException("Customer not found");
+        }else {
+            customerDAO.deleteById(cusId);
+        }
     }
 
     @Override
     public void updateCustomer(String cusId, CustomerDTO customerDTO) {
-
+        Optional<CustomerEntity> findCustomer = customerDAO.findById(cusId);
+        if (!findCustomer.isPresent()) {
+            throw new CustomerNotFoundException("Customer not found");
+        }else {
+            findCustomer.get().setCusName(customerDTO.getCusName());
+            findCustomer.get().setCusAddress(customerDTO.getCusAddress());
+            findCustomer.get().setCusTeleNo(customerDTO.getCusTeleNo());
+        }
     }
 }
