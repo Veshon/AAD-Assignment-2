d92f896f3974fc753621df766fd91480c4169e98
veshon <vashonmabima12@gmail.com>
2024-10-04 18:55:56 +0530

    Generated Order Id

--- src/main/java/com/example/assignment2/util/AppUtil.java
+++ src/main/java/com/example/assignment2/util/AppUtil.java
@@ -12,6 +12,9 @@ public class AppUtil {
     public static String generateItemId(){
         return "ITEM-" + UUID.randomUUID();
     }
+    public static String generateOrderId(){
+        return "ORDER-" + UUID.randomUUID();
+    }
 
     public static String profilePicToBase64(byte[] profilePic){
         return Base64.getEncoder().encodeToString(profilePic);
